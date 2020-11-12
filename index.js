c36c5960271ff3d8c73e3df3bdbd8f51c9f67297
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-12 12:45:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards (array, event){
+    let result=[]
+    for (let i=0; i< array.length; i++){
+        result.push (`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return result;
+} 
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0 ) {
+        console.log(i);
+        i --;
+    }     
+}
